9a4ac5375e92c72efc2dc89c84973cd7e65b6c45
rafikidevfs <rafikidevfs@gmail.com>
2026-06-28 18:45:33 -0300

    Update next.config.js for experimental settings
    Refactor next.config.js to include experimental turbopack settings.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,24 @@
-const path = require("path");
+// const path = require("path");
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: { unoptimized: true },
+//   turbopack: {
+//     root: path.resolve(__dirname),
+//   },
+// };
+
+// module.exports = nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { unoptimized: true },
-  turbopack: {
-    root: path.resolve(__dirname),
+  images: { 
+    unoptimized: true 
   },
+  experimental: {
+    turbopack: {}
+  }
 };
 
 module.exports = nextConfig;
+
