f5ff59869d0da8274845660ab818fbb4b7187945
rafikidevfs <rafikidevfs@gmail.com>
2026-06-29 13:18:08 -0300

    fix: correção do next config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,15 @@
-// const path = require("path");
+const path = require("path");
 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: { unoptimized: true },
-//   turbopack: {
-//     root: path.resolve(__dirname),
-//   },
-// };
-
-// module.exports = nextConfig;
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   images: { unoptimized: true },
-=======
-  images: { 
-    unoptimized: true 
+  turbopack: {
+    root: path.resolve(__dirname),
   },
-  experimental: {
-    turbopack: {}
-  }
->>>>>>> 22da3212451c4e1c0316e81df9e4d8fcc8eccf1c
 };
 
 module.exports = nextConfig;
+
+
+
 
